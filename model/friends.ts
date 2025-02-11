c05376a55974e2ee94e7c8e35b760361a2b9e15a
Pilks-pixel <peterpilkington@hotmail.com>
2025-02-11 20:09:42 +0000

    model created

---
+++ model/friends.ts
@@ -0,0 +1,22 @@
+type Friend = {
+  id: number;
+  name: string;
+};
+
+// Type definition
+const friends: Friend[] = [
+  {
+    id: 0,
+    name: "Nikola Tesla",
+  },
+  {
+    id: 1,
+    name: "Sir Isaac Newton",
+  },
+  {
+    id: 2,
+    name: "Albert Einstein",
+  },
+];
+
+export default friends;
--- server.ts
+++ server.ts
@@ -1,45 +1,22 @@
 // const express = require("express");
 import express from "express";
+
+import friends from "./model/friends.ts";
 const app = express();
 const port = 3000;
 
-type Friend = {
-  id: number;
-  name: string;
-};
-
-// Type definition
-const friends: Friend[] = [
-  {
-    id: 0,
-    name: "Nikola Tesla",
-  },
-  {
-    id: 1,
-    name: "Sir Isaac Newton",
-  },
-  {
-    id: 2,
-    name: "Albert Einstein",
-  },
-];
-
 // Middleware
-const logTime = function setTime(req, res, next) {
+function logTime(req, res, next) {
   req.time = Number(new Date());
   console.log(`Request received at ${req.time}`);
   next();
-};
-
-function getTotalTime(req, res, next) {
   res.time = Number(new Date()) - req.time;
   console.log(`Total time taken: ${res.time}ms`);
-  next();
 }
 
 app.use(express.json());
 app.use(logTime);
-app.use(getTotalTime);
+// app.use(getTotalTime);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -58,7 +35,7 @@ app.get("/friends/:id", (req, res) => {
   if (!friend) {
     res.status(404).json("Friend not found");
   } else {
-    res.json(friend.name);
+    res.status(200).json(friend.name);
   }
 });
 
